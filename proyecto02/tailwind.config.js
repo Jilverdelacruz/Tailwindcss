8b925a2743befbdcc40f10a85139eba2bc6d1ea2
Jilver de la Cruz <jilver.practicando@gmail.com>
2021-04-02 08:06:56 -0500

    Creando header

---
+++ proyecto02/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily:{
+      mont:['Montserrat'],
+     
+    },
+    extend: {
+      colors:{
+        primary:'#00957C',
+        secundary:'#FEAF04',
+        fondo:'#F9FBFF',
+        texto:'#3F3D56',
+      }
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
