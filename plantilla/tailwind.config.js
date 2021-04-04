9849e29429a609d3c7f56970fdfe596125a5e91b
Jilver de la Cruz <jilver.practicando@gmail.com>
2021-04-04 18:27:19 -0500

    primer avance

---
+++ plantilla/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary:'#3699ff'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
